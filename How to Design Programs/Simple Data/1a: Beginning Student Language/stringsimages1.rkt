6dfb830e7859032f370e23e183c86139a0bb8be0
el634dev <84924260+el634dev@users.noreply.github.com>
2022-10-27 00:33:33 -0400

    Create stringsimages1.rkt

---
+++ How to Design Programs/Simple Data/1a: Beginning Student Language/stringsimages1.rkt
@@ -0,0 +1,13 @@
+;"apple"
+;(string-append "Ada" " " "Lovelace")
+
+;"123"
+;(+ 1 123)
+;"123"
+
+;(string-length "apple")
+;(substring "Caribou" 2 4)
+          ;"012345678" ; indexes
+          
+;(substring "012345678" 2 4)
+;(substring "Caribou" 0 3)
